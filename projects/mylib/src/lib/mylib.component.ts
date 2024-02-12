d1579580125f308206fed61ea3f2bfad2b992cfa
Deepti <diptisuyogindia@gmail.com>
2024-02-12 15:01:33 +0530

    new

---
+++ projects/mylib/src/lib/mylib.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-mylib',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      mylib works!
+    </p>
+  `,
+  styles: ``
+})
+export class MylibComponent {
+
+}
